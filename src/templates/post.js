4a04b15c412334c6cdc036542fb441f65521d42e
nami2012 <namithasadeeshbabu@gmail.com>
2020-06-11 15:29:10 +0530

    Fix post issues

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,16 +2,25 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
 
+import Header from "../components/header"
+
+import Footer from "../components/footer"
+import StyledFullBackground from "../components/fullbackground"
+
 export default function Template({data}){
     const {markdownRemark:post} = data;
     return (
-        <div>
-            <h1>
-                {post.frontmatter.title}
+        <StyledFullBackground>
+            
+             <div>
+                <h1>
+                    {post.frontmatter.title}
                     <div dangerouslySetInnerHTML={{__html:post.html}}/>
-            </h1>
-        </div>
-    )
+                </h1>
+             </div>
+            <Footer/>
+        </StyledFullBackground>
+        )
 }
 
 export const postQuery = graphql`
